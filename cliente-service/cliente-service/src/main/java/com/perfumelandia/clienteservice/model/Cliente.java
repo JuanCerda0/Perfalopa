6ae447ee6ea69fb0c05c1d4efbc9a71da7a960ba
Santiago Eduardo Palomé Córdova <sa.palome@duocuc.cl>
2025-05-06 03:43:30 -0400

    Add files via upload

---
+++ cliente-service/cliente-service/src/main/java/com/perfumelandia/clienteservice/model/Cliente.java
@@ -0,0 +1,69 @@
+package com.perfumelandia.clienteservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String direccion;
+
+    public Cliente() {}
+
+    public Cliente(String correo, String direccion, long id, String nombre, String telefono) {
+        this.correo = correo;
+        this.direccion = direccion;
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
