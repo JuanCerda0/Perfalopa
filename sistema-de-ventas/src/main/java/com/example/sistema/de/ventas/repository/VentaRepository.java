9b6ad5ee9f28d94b1d28147edb1560aa0f64b5f9
Juan Cerda <167485913+JuanCerda0@users.noreply.github.com>
2025-05-05 20:15:14 -0400

    Delete sistema-de-ventas directory
    Jordan culiao sube las weas bien

--- sistema-de-ventas/src/main/java/com/example/sistema/de/ventas/repository/VentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema.de.ventas.repository;
-
-import com.example.sistema.de.ventas.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VentaRepository extends JpaRepository<Venta, Long> {
-}
