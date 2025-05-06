adc5561e9a89b33d1626ec512def2b8264979c15
Juan Cerda <167485913+JuanCerda0@users.noreply.github.com>
2025-05-05 20:17:33 -0400

    Delete src directory

--- src/main/java/com/example/sistema/de/ventas/repository/FacturaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema.de.ventas.repository;
-
-import com.example.sistema.de.ventas.model.Factura;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacturaRepository extends JpaRepository<Factura, Long> {
-}
