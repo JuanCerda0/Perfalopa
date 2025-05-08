ffbe125a534023cd48830f32737e9cf8dcc67ac4
Juan Cerda <ju.cerdar@duocuc.cl>
2025-05-07 20:47:04 -0400

    Se arreglo el error relacionado a dependencias, ahora hay 3 errores mas. This is fine

--- inventario-service/src/main/java/com/perfulandia/inventario_service/model/Producto.java
+++ inventario-service/src/main/java/com/perfulandia/inventario_service/model/Producto.java
@@ -1,10 +1,9 @@
 package com.perfulandia.inventario_service.model;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Producto {
