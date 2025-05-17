64557683294dfa22dc6494f64fd47a1d82417665
Jordan <jo.villegasm@duocuc.cl>
2025-05-17 09:22:14 -0400

    Microservicio venta funcional

--- sistema-de-ventas/src/main/java/com/example/sistema/de/ventas/repository/DetalleVentaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema.de.ventas.repository;
-
-import com.example.sistema.de.ventas.model.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
--- sistema-de-ventas/src/main/java/com/example/sistema/de/ventas/repository/DevolucionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sistema.de.ventas.repository;
-
-import com.example.sistema.de.ventas.model.Devolucion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DevolucionRepository extends JpaRepository<Devolucion, Long> {
-}
